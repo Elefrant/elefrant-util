63f4cfac908ef0c77e5528171bfad02ef8d9c9c5
Marcos Sanz <marcos.sanz@13genius.com>
2014-11-14 13:34:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
 	async: require('async'),
 	lodash: require('lodash'),
 	chalk: require('chalk'),
+	q: require('q'),
 	glob: require('glob'),
 	format: require('./lib/format')
 };
